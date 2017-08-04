3f968c45976919b14fc3804adc51a405fcb259b6
Salleh Jahaf <salj4215@gmail.com>
2017-08-03 23:20:04 -0400

    Add files via upload

---
+++ homework3/homework3.sh
@@ -0,0 +1,8 @@
+echo "**************************************"
+echo "************Homework 3 script***************"
+# get my name from the password file
+cat /etc/passwd |grep $USER
+# print the following text from the other file
+cat homework3.txt
+
+# end of file
